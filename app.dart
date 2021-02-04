1b0f50f8cf1557bf2d23b7a6b15a2220cacde8b9
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-02-04 20:53:07 -0300

    Commit 2 dos arquivos na branch master

--- app.dart
+++ app.dart
@@ -2,9 +2,31 @@ num soma(num x1, num x2) {
   return (x1 + x2);
 }
 
+num subtracao(num x1, num x2) {
+  return (x1 - x2);
+}
+
+num multiplicacao(num x1, num x2) {
+  return (x1 * x2);
+}
+
+num divisao(num x1, num x2) {
+  return (x1 / x2);
+}
+
 main(List<String> args) {
   num x1 = 2;
   num x2 = 2;
-  num resultado = soma(x1, x2);
-  print(resultado);
+
+  num resultadoSoma = soma(x1, x2);
+  print(resultadoSoma);
+
+  num resultadoSubtracao = subtracao(x1, x2);
+  print(resultadoSubtracao);
+
+  num resultadoMultiplicacao = multiplicacao(x1, x2);
+  print(resultadoMultiplicacao);
+
+  num resultadoDivisao = divisao(x1, x2);
+  print(resultadoDivisao);
 }
